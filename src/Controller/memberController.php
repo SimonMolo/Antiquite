9cb8a27bbd024613b3c4888faf3813b42b2ca240
SimonMolo <lemartelotsimon@gmail.com>
2022-12-26 12:27:36 +0100

    probleme memberController.php

--- src/Controller/memberController.php
+++ src/Controller/memberController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Form\UserType;
+use App\Form\membersType;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,17 +11,42 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
 
 class memberController extends AbstractController
 {
 
+    /**
+     * @Route("/login", name="login")
+     */
+    public function index(AuthenticationUtils $authenticationUtils) : Response
+    {
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+        return $this->render('members/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
+
+    }
+
+    /**
+     * @Route("/logout", name="app_logout", methods={"GET"})
+     */
+    public function logout(): void
+    {
+        // controller can be blank: it will never be called!
+        throw new \Exception('Don\'t forget to activate logout in security.yaml');
+    }
+
         /**
-        * @Route("/register", name="app_register")
+         * @Route("/register", name="app_register")
          */
         public function register(Request $request, UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $entityManager): Response
     {
         $user = new User();
-        $form = $this->createForm(UserType::class, $user);
+        $form = $this->createForm(membersType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
@@ -29,21 +54,23 @@ class memberController extends AbstractController
 
             // ... e.g. get the user data from a registration form
             $user = new User();
-            $plaintextPassword = $form->get('plainPassword')->getData();
-
-
+            $plaintextPassword = $form->get('password')->getData();
             // hash the password (based on the security.yaml config for the $user class)
             $hashedPassword = $passwordHasher->hashPassword(
                 $user,
                 $plaintextPassword
             );
+
             $user->setPassword($hashedPassword);
             $user->setEmail($form->get('email')->getData());
+            $user->setFirstname($form->get('firstname')->getData());
+            $user->setFirstname($form->get('name')->getData());
+
             $entityManager->persist($user);
             $entityManager->flush();
             // do anything else you need here, like send an email
 
-            return $this->redirect('');
+            return $this->redirect('members/register.html.twig');
         }
 
         return $this->render('members/register.html.twig', [
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,6 +4,8 @@ namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 class User
@@ -13,7 +15,7 @@ class User
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 255, unique: true)]
     private ?string $name = null;
 
     #[ORM\Column(length: 255)]
@@ -57,6 +59,9 @@ class User
         return $this;
     }
 
+    /**
+     * @return string
+     */
     public function getPassword(): ?string
     {
         return $this->password;
@@ -71,7 +76,11 @@ class User
 
     public function getRole(): array
     {
-        return $this->role;
+        $role = $this->role;
+        // guarantee every user at least has ROLE_USER
+        $role[] = 'ROLE_USER';
+
+        return array_unique($role);
     }
 
     public function setRole(array $role): self
