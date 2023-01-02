da6593e6b3917041d84239e86cdc3792bb612e2a
SimonMolo <lemartelotsimon@gmail.com>
2023-01-02 15:07:32 +0100

    loginform Test

--- src/Controller/memberController.php
+++ src/Controller/memberController.php
@@ -32,14 +32,6 @@ class memberController extends AbstractController
 
     }
 
-    /**
-     * @Route("/logout", name="app_logout", methods={"GET"})
-     */
-    public function logout(): void
-    {
-        // controller can be blank: it will never be called!
-        throw new \Exception('Don\'t forget to activate logout in security.yaml');
-    }
 
         /**
          * @Route("/register", name="app_register")
